91fbadc0cf9504ab33bc9679639bfdcb73eda48c
Ishant Dahiya <42692924+ishantd@users.noreply.github.com>
2019-09-13 00:26:01 +0530

    Web App with Authentication complete! (WITHOUT IMAGE)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth')
 
+//This is the welcome page
 router.get('/', (req, res)=> res.render('welcome'))
+
+//This is the dashboard for the logged in user
+router.get('/dashboard', ensureAuthenticated,  (req, res) => 
+res.render('dashboard', {
+    name: req.user.name
+}))
 
 module.exports = router;
